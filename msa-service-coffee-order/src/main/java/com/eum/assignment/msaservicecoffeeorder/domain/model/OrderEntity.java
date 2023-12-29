72a4ae8a3724de769cd32bfe3dc90227970f3f13
jykim-rust <alexj99@naver.com>
2023-12-29 21:31:16 +0900

    :sparkles: OrderEntity 추가

---
+++ msa-service-coffee-order/src/main/java/com/eum/assignment/msaservicecoffeeorder/domain/model/OrderEntity.java
@@ -0,0 +1,14 @@
+package com.eum.assignment.msaservicecoffeeorder.domain.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class OrderEntity {
+    private String id;
+    private String orderNumber;
+    private String coffeeName;
+    private String coffeeCount;
+    private String customerName;
+}
