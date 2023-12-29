4f17620ccd43df37b3173952bc708025ea401439
jykim-rust <alexj99@naver.com>
2023-12-29 21:23:49 +0900

    :sparkles: 주문이 들어오면 CVO를 저장해주고 ID를 반환해주는 CoffeeOrder서비스 구현

---
+++ msa-service-coffee-order/src/main/java/com/eum/assignment/msaservicecoffeeorder/domain/service/CoffeeOrder.java
@@ -0,0 +1,26 @@
+package com.eum.assignment.msaservicecoffeeorder.domain.service;
+
+import com.eum.assignment.msaservicecoffeeorder.domain.model.CoffeeOrderCVO;
+import com.eum.assignment.msaservicecoffeeorder.domain.model.OrderEntity;
+import com.eum.assignment.msaservicecoffeeorder.domain.repository.ICoffeeOrderRepository;
+
+public class CoffeeOrder implements ICoffeeOrder{
+    private final ICoffeeOrderRepository iCoffeeOrderRepository;
+
+    public CoffeeOrder(ICoffeeOrderRepository iCoffeeOrderRepository) {
+        this.iCoffeeOrderRepository = iCoffeeOrderRepository;
+    }
+
+    @Override
+    public String coffeeOrder(CoffeeOrderCVO coffeeOrderCVO) {
+        OrderEntity orderEntity = OrderEntity.builder()
+                .orderNumber(coffeeOrderCVO.getOrderNumber())
+                .coffeeName(coffeeOrderCVO.getCoffeeName())
+                .coffeeCount(coffeeOrderCVO.getCoffeeCount())
+                .customerName(coffeeOrderCVO.getCustomerName())
+                .build();
+        iCoffeeOrderRepository.coffeeOrderSave(orderEntity);
+
+        return orderEntity.getId();
+    }
+}
