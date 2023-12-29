d3cdd8608049fcf5e33ce0498053510387592854
jykim-rust <alexj99@naver.com>
2023-12-29 21:28:40 +0900

    :sparkles: CoffeeOrder 인터페이스 생성

---
+++ msa-service-coffee-order/src/main/java/com/eum/assignment/msaservicecoffeeorder/domain/service/ICoffeeOrder.java
@@ -0,0 +1,8 @@
+package com.eum.assignment.msaservicecoffeeorder.domain.service;
+
+import com.eum.assignment.msaservicecoffeeorder.domain.model.CoffeeOrderCVO;
+
+public interface ICoffeeOrder {
+
+    public String coffeeOrder(CoffeeOrderCVO coffeeOrderCVO);
+}
