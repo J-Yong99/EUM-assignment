681b20a1b71bc083f17d64046646f827a83bba0b
jykim-rust <alexj99@naver.com>
2023-12-29 21:32:44 +0900

    :sparkles: OrderEntityJPO 추가

---
+++ msa-service-coffee-order/src/main/java/com/eum/assignment/msaservicecoffeeorder/springboot/repository/jpa/OrderEntityJPO.java
@@ -0,0 +1,23 @@
+package com.eum.assignment.msaservicecoffeeorder.springboot.repository.jpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEntityJPO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String orderNumber;
+    private String coffeeName;
+    private String coffeeCount;
+    private String customerName;
+}
