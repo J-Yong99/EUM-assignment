1a431609761487ba67316ef7fad12f02ffbf796f
jykim-rust <alexj99@naver.com>
2023-12-29 21:31:08 +0900

    :sparkles: MemberDVO 추가

---
+++ msa-service-coffee-member/src/main/java/com/eum/assignment/msaservicecoffeemember/springboot/repository/dvo/MemberDVO.java
@@ -0,0 +1,9 @@
+package com.eum.assignment.msaservicecoffeemember.springboot.repository.dvo;
+
+import lombok.Data;
+
+@Data
+public class MemberDVO {
+    private int id;
+    private String memberName;
+}
