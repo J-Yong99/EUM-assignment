090f2da75335ad90acbdaf05188cf01033c69c4e
jykim-rust <alexj99@naver.com>
2023-12-29 21:28:56 +0900

    :sparkles: CoffeeOrder 저장해줄 JPA레포 추가

---
+++ msa-service-coffee-order/src/main/java/com/eum/assignment/msaservicecoffeeorder/springboot/repository/jpa/ICoffeeOrderJpaRepository.java
@@ -0,0 +1,9 @@
+package com.eum.assignment.msaservicecoffeeorder.springboot.repository.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICoffeeOrderJpaRepository extends JpaRepository<OrderEntityJPO, String> {
+
+}
