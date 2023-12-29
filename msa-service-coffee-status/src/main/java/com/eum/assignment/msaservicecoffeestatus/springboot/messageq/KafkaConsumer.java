7b0da49c8fb998ffb9d304aff92cfc10fbe23d54
jykim-rust <alexj99@naver.com>
2023-12-29 21:30:12 +0900

    :sparkles: 큐에서 받아올 컨슈머 구현

---
+++ msa-service-coffee-status/src/main/java/com/eum/assignment/msaservicecoffeestatus/springboot/messageq/KafkaConsumer.java
@@ -0,0 +1,22 @@
+package com.eum.assignment.msaservicecoffeestatus.springboot.messageq;
+
+import com.eum.assignment.msaservicecoffeestatus.springboot.repository.ICoffeeStatusMapper;
+import com.eum.assignment.msaservicecoffeestatus.springboot.repository.dvo.OrderStatusDVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    @Autowired
+    ICoffeeStatusMapper iCoffeeStatusMapper;
+
+    @KafkaListener(topics = "kafka-test")
+    public void processMessage(String kafkaMessage) {
+        System.out.println("kafkaMessage: " + kafkaMessage);
+        OrderStatusDVO orderStatusDVO = new OrderStatusDVO();
+        orderStatusDVO.setOrderHistory(kafkaMessage);
+
+        iCoffeeStatusMapper.insertCoffeeOrderStatus(orderStatusDVO);
+    }
+}
