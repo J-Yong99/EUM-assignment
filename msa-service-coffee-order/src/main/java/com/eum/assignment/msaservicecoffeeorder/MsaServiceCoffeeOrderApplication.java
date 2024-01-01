d2a86fc7abebd974080bedd917013c41d02c317d
jykim-rust <alexj99@naver.com>
2024-01-01 17:44:44 +0900

    :sparkles: 유레카서버가 탐색가능하게 어노테이션 추가

--- msa-service-coffee-order/src/main/java/com/eum/assignment/msaservicecoffeeorder/MsaServiceCoffeeOrderApplication.java
+++ msa-service-coffee-order/src/main/java/com/eum/assignment/msaservicecoffeeorder/MsaServiceCoffeeOrderApplication.java
@@ -2,11 +2,12 @@ package com.eum.assignment.msaservicecoffeeorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsaServiceCoffeeOrderApplication {
 
