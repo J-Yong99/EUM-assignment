8c20ce2b7ba5370ed7414223300c7a3eed64a404
jykim-rust <alexj99@naver.com>
2024-01-01 17:38:34 +0900

    :adhesive_bandage: 필요없는 메소드 삭제 및 메소드 반환값 변경

--- msa-service-coffee-status/src/main/java/com/eum/assignment/msaservicecoffeestatus/springboot/rest/CoffeeOrderStatusRestController.java
+++ msa-service-coffee-status/src/main/java/com/eum/assignment/msaservicecoffeestatus/springboot/rest/CoffeeOrderStatusRestController.java
@@ -1,28 +1,25 @@
 package com.eum.assignment.msaservicecoffeestatus.springboot.rest;
 
-import com.eum.assignment.msaservicecoffeestatus.springboot.repository.ICoffeeStatusMapper;
-import com.eum.assignment.msaservicecoffeestatus.springboot.repository.dvo.OrderStatusDVO;
+import com.eum.assignment.msaservicecoffeestatus.springboot.repository.jpa.IOrderStatusJpaRepository;
+import com.eum.assignment.msaservicecoffeestatus.springboot.repository.jpa.OrderStatus;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CoffeeOrderStatusRestController {
     @Autowired
-    ICoffeeStatusMapper iCoffeeStatusMapper;
+    private IOrderStatusJpaRepository iOrderStatusJpaRepository;
 
     @HystrixCommand
-    @PostMapping("/createOrderStatus")
-    public ResponseEntity<OrderStatusDVO> coffeeOrderStatus() {
-        OrderStatusDVO orderStatusDVO = iCoffeeStatusMapper.selectCoffeeOrderStatus();
-        return ResponseEntity.ok(orderStatusDVO);
+    @GetMapping("/coffeeOrderStatus")
+    public ResponseEntity<List<OrderStatus>> coffeeOrderStatus() {
+        return ResponseEntity.ok(iOrderStatusJpaRepository.findAll());
     }
 
-    @PutMapping("/createStatusTable")
-    public void createStatusTable() {
-        iCoffeeStatusMapper.createStatusTable();
-    }
 }
