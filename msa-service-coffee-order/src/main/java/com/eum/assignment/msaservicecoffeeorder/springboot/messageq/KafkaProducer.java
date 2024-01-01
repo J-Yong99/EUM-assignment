0ef1fd86159066dd316f83f4052371f2e9707797
jykim-rust <alexj99@naver.com>
2024-01-01 17:43:44 +0900

    :adhesive_bandage: 전송타입을 객체가 아닌 String으로 변환

--- msa-service-coffee-order/src/main/java/com/eum/assignment/msaservicecoffeeorder/springboot/messageq/KafkaProducer.java
+++ msa-service-coffee-order/src/main/java/com/eum/assignment/msaservicecoffeeorder/springboot/messageq/KafkaProducer.java
@@ -3,18 +3,23 @@ package com.eum.assignment.msaservicecoffeeorder.springboot.messageq;
 import com.eum.assignment.msaservicecoffeeorder.domain.model.CoffeeOrderCVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 @Slf4j
 public class KafkaProducer {
-    private final KafkaTemplate<String, CoffeeOrderCVO> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     public void send(String topic, CoffeeOrderCVO payload) {
         log.info("sending payload='{}' to topic='{}'", payload, topic);
-        kafkaTemplate.send(topic, payload);
+        // payload를 String으로 변환하여 전송
+        kafkaTemplate.send(topic, payload.toString());
     }
 
 }
